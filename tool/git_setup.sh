5edb0df281413ed448c9da97250f64c670b4e300
luchaodong <luchaodong@meizu.com>
2016-06-23 11:48:01 +0800

    git setup script

---
+++ tool/git_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+
+sudo apt-get install git gitk meld
+
+touch ~/.gitignore
+echo "cscope*" > ~/.gitignore
+echo "tag*" >> ~/.gitignore
+echo "*.swp" >> ~/.gitignore
+
+git config --global core.excludesfile ~/.gitignore
+git config --global user.name luchaodong
+git config --global usr.email lcdsdream@126.com
+
+git config --list
+
