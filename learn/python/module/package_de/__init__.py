215d8aa0b629d0665d54aa8c2b938007c273a650
orientlu <lcdsdream@126.com>
2017-08-25 21:09:25 -0700

    python package&module

---
+++ learn/python/module/package_de/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+# by orientlu
+
+# import sub module here
+# when import module_de
+# will auto import it's submodule here
+
+from . import module_d
+from .module_e import module_e_fun
+
+if __name__ == "__main__":
+   pass
