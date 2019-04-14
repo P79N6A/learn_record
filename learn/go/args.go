a285d0a06861708059d1e8af93d049540944946f
orientlu <orientlu@tencent.com>
2019-04-14 16:34:47 +0800

    how to import self define go package

---
+++ learn/go/args.go
@@ -0,0 +1,14 @@
+//
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var str string
+	str = strings.Join(os.Args[1:], " ")
+	fmt.Println(str)
+}
--- learn/go/package_test/test_pack.go
+++ learn/go/package_test/test_pack.go
@@ -1,18 +1,21 @@
 package main
 
-
 import (
-    "fmt"
-    "./pack1" //　自定义包路径，目录对应包名
+	"./pack1" //　自定义包路径，目录对应包名
+	// 调试时方便使用上相对路径导入包ok
+	// 正式部署建议使用如下，不要使用相对路径，直接导入
+	// 并且把projiect放置于 $GOPATH/src 目录下
+	//"pack1"
+	//"xx/pack2"
+	"fmt"
 )
 
-
 func main() {
-    testStr := pack1.ReturnStr2()
-    fmt.Println(testStr)
+	testStr := pack1.ReturnStr2()
+	fmt.Println(testStr)
 
-    fmt.Println(pack1.PackInt)
-    fmt.Println(pack1.PackInt2)
-    fmt.Println(pack1.PackFloat2)
-   // fmt.Println(pack1.localString)
+	fmt.Println(pack1.PackInt)
+	fmt.Println(pack1.PackInt2)
+	fmt.Println(pack1.PackFloat2)
+	// fmt.Println(pack1.localString)
 }
