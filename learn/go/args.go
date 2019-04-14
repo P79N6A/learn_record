6ae87385e1eb9bdfeedf78683bbe16f2c790bcd7
orientlu <orientlu@tencent.com>
2019-04-14 19:52:31 +0800

    go args flag

--- learn/go/args.go
+++ learn/go/args.go
@@ -2,13 +2,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
+
+var port *int = flag.Int("port", 1000, "server port")
+var ip *string = flag.String("ip", "127.0.0.1", "server ip")
+
 func main() {
 	var str string
 	str = strings.Join(os.Args[1:], " ")
 	fmt.Println(str)
+
+	flag.Parse()
+    fmt.Println(*ip, " : ", *port)
 }
